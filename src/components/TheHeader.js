d611f34f30f7edc3a5d8051afecd159c1bfc8687
whdgk0602 <whdgk0602@naver.com>
2025-08-14 15:13:40 +0900

    create project

---
+++ src/components/TheHeader.js
@@ -0,0 +1,54 @@
+import { Component } from "../core/heropy";
+
+export default class TheHeader extends Component{
+	constructor(){
+		super({
+			tagName : 'header',
+			state : {
+				menus : [
+					{
+						name : 'Search',
+						href : '#/'
+					},
+					{
+						name : 'Movie',
+						href : '#/movie?id=tt4520988'
+					},
+					{
+						name : 'About',
+						href : '#/about'
+					}
+				]
+			}
+		})
+
+		window.addEventListener('popstate', ()=>{
+			this.render();
+		})
+	}
+	render(){
+		this.el.innerHTML = /*html*/`
+			<a href="#/" class=logo><span>OMDbAPI</span>.COM</a>
+			<nav>
+				<ul>
+					${this.state.menus.map(menu=>{
+						const href = menu.href.split('?')[0];
+						const hash = location.hash.split('?')[0];
+						const isActive = href === hash
+						return /*html*/`
+							<li>
+								<a
+									class="${isActive ? 'active' : ''}"
+									href="${menu.href}">${menu.name}
+								</a>
+							</li>
+						`
+					}).join('')}
+				</ul>
+			</nav>
+			<a href="#/about" class="user">
+				<img src="https://img1.daumcdn.net/thumb/C360x360/?scode=mtistory2&fname=https%3A%2F%2Ftistory1.daumcdn.net%2Ftistory%2F7604534%2Fattach%2F0e3e0211fc7a48a295e65b10a568f6c5" alt="User"/>
+			</a>
+		`
+	}
+}
---
+++ src/store/about.js
@@ -0,0 +1,10 @@
+import { Store } from "../core/heropy";
+
+export default new Store ({
+	photo : "https://img1.daumcdn.net/thumb/C360x360/?scode=mtistory2&fname=https%3A%2F%2Ftistory1.daumcdn.net%2Ftistory%2F7604534%2Fattach%2F0e3e0211fc7a48a295e65b10a568f6c5",
+	name : 'Keder / KimJongHa',
+	email : "whdgk0602@naver.com",
+	blog : "https://keder-tips.tistory.com/",
+	gitHub : "https://github.com/whdgk0602",
+	repository : 'https://github.com/whdgk0602/vanilla-movie-app',
+})
